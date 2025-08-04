316a7cb9fc0f83c4d5105b526fe1cd1d46ceb596
Rapaka Karthik <rapakakarthik7@gmail.com>
2025-08-04 17:54:25 +0530

    changes

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "karthik-rg-demo"
+  location = "East US"
+}
---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
